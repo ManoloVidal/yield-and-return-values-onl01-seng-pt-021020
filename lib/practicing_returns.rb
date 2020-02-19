09426ebf0826a204116432e1d9bf11c4a66d8b6a
Juan Vidal-Ramos <vidalramosjm@gmail.com>
2020-02-19 18:34:26 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -4,9 +4,9 @@ def hello(array)
   i = 0
   while i < array.length
   #binding.pry 
-    yield(array[i])
+    collection << yield(array[i])
     i += 1
   end
 end
-binding.pry 
+#binding.pry 
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
